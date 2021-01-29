050d564b51cd8d39d31d9a3de12a442e512d9427
Sebastian Ciesielski <sebastianciesielski94@gmail.com>
2021-01-29 16:57:38 +0100

    POJO Data Model. Closes #2.

---
+++ src/main/java/io/github/ciesielskis/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package io.github.ciesielskis.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/io/github/ciesielskis/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package io.github.ciesielskis.petclinic.model;
+
+public class Vet extends Person {
+}
