8a9bc620e61446713a0bf41f4cd1e56865079928
Sebastian Ciesielski <sebastianciesielski94@gmail.com>
2021-01-31 18:48:41 +0100

    Refactored services to common interface. Closes #10.

---
+++ pet-clinic-data/src/main/java/io/github/ciesielskis/petclinic/services/CrudService.java
@@ -0,0 +1,14 @@
+package io.github.ciesielskis.petclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    T findById(ID id);
+    Set<T> findAll();
+
+    T save(T object);
+
+    void delete(T object);
+    void deleteById(ID id);
+}
--- pet-clinic-data/src/main/java/io/github/ciesielskis/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/io/github/ciesielskis/petclinic/services/PetService.java
@@ -2,11 +2,6 @@ package io.github.ciesielskis.petclinic.services;
 
 import io.github.ciesielskis.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
