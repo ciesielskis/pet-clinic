fe2c316ddbeda763f3f3aa4de1c7d4b8c5191042
Sebastian Ciesielski <sebastianciesielski94@gmail.com>
2021-02-02 14:25:28 +0100

    Auto Generate Map IDs. Closes #22.

--- pet-clinic-data/src/main/java/io/github/ciesielskis/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/io/github/ciesielskis/petclinic/services/map/AbstractMapService.java
@@ -1,32 +1,51 @@
 package io.github.ciesielskis.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import io.github.ciesielskis.petclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    public T findById(ID id) {
+    protected Map<Long, T> map = new HashMap<>();
+
+    T findById(ID id) {
         return map.get(id);
     }
 
-    public Set<T> findAll() {
+    Set<T> findAll() {
         return new HashSet<>(map.values());
     }
 
-    public T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+
+        if(object != null) {
+            if(object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+
+            throw new RuntimeException("Object cannot be null.");
+        }
+
         return object;
     }
 
-    public void delete(T object) {
+    void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
 
-    public void deleteById(ID id) {
+    void deleteById(ID id) {
         map.remove(id);
+    }
+
+    private Long getNextId() {
+        Long nextId;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
--- pet-clinic-web/src/main/java/io/github/ciesielskis/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/io/github/ciesielskis/petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import io.github.ciesielskis.petclinic.model.Owner;
 import io.github.ciesielskis.petclinic.model.Vet;
 import io.github.ciesielskis.petclinic.services.OwnerService;
 import io.github.ciesielskis.petclinic.services.VetService;
-import io.github.ciesielskis.petclinic.services.map.OwnerMapService;
-import io.github.ciesielskis.petclinic.services.map.VetMapService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -24,7 +22,6 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
         //owner1.setAddress("123 Brickerel");
@@ -34,7 +31,6 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
         //owner2.setAddress("123 Brickerel");
@@ -44,7 +40,6 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
         //vet1.getSpecialities().add(savedRadiology);
@@ -52,7 +47,6 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
         //vet2.getSpecialities().add(savedSurgery);
